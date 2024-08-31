d2e4076b82f567c92e82187c68a40a23ed466b6c
Badhri007 <badhrisambath2003@gmail.com>
2024-08-31 22:18:58 +0530

    Changed localstorage

--- controllers/influencerLoginController.js
+++ controllers/influencerLoginController.js
@@ -1,18 +1,18 @@
-const mongoose=require('mongoose')
-const Influencers=require('../models/influencerModel')
-const bcrypt=require('bcryptjs')
+const mongoose = require('mongoose')
+const Influencers = require('../models/influencerModel')
+const bcrypt = require('bcryptjs')
 
 
-const influencerLogin=async(req,res)=>{
-    
-    const {email,password}=req.body;
-    const influencer=await Influencers.findOne({email:email});
+const influencerLogin = async (req, res) => {
+
+    const { email, password } = req.body;
+    const influencer = await Influencers.findOne({ email: email });
     if (influencer && await bcrypt.compare(password, influencer.password)) {
-        return res.status(200).json({ success: true, userId: influencer._id });
+        return res.status(200).json({ success: true, influencerId: influencer._id });
     } else {
         return res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
 
 }
 
-module.exports={influencerLogin}
+module.exports = { influencerLogin }
--- controllers/sponsorLoginController.js
+++ controllers/sponsorLoginController.js
@@ -1,18 +1,18 @@
-const mongoose=require('mongoose')
-const Sponsors=require('../models/sponsorModel')
-const bcrypt=require('bcryptjs')
+const mongoose = require('mongoose')
+const Sponsors = require('../models/sponsorModel')
+const bcrypt = require('bcryptjs')
 
 
-const sponsorLogin=async(req,res)=>{
-    
-    const {email,password}=req.body;
-    const sponsor=await Sponsors.findOne({email:email});
+const sponsorLogin = async (req, res) => {
+
+    const { email, password } = req.body;
+    const sponsor = await Sponsors.findOne({ email: email });
     if (sponsor && await bcrypt.compare(password, sponsor.password)) {
-        return res.status(200).json({ success: true, userId: sponsor._id });
+        return res.status(200).json({ success: true, sponsorId: sponsor._id });
     } else {
         return res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
 
 }
 
-module.exports={sponsorLogin}
+module.exports = { sponsorLogin }
